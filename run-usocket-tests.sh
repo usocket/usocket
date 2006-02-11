6c0a164d834c9473b519993bd16f8dc7d6391203
ehuelsmann <ehuelsmann@423ce6ab-4d0b-0410-a604-eef82375bc58>
2006-02-11 09:20:56 +0000

    Rearrange and extend test scripts.

--- run-usocket-tests.sh
+++ run-usocket-tests.sh
@@ -8,15 +8,34 @@
 # I need to figure out how to setup ASDF with the other lisps
 # I have installed: cmucl, ABCL, clisp, allegro and lispworks
 
-for my_lisp in sbcl ; do
+cd `dirname $0`/test
+
+if test -z "$1" ; then
+  lisps="sbcl clisp $HOME/src/acl/acl70_trial/alisp"
+else
+  lisps=$1
+fi
+
+for my_lisp in $lisps ; do
+
+# *.fasl are used by both sbcl and allegro,
+# so remove them to prevent choking either
+
+args=
+if test "`basename $my_lisp`" == "alisp" ; then
+  args=-batch
+fi
 
 echo "
-(require 'usocket-test)
+#-sbcl (load \"asdf.lisp\")
+
+(asdf:operate #-sbcl 'asdf:load-source-op
+              #+sbcl 'asdf:load-op :usocket-test)
 
 (usocket-test:do-tests)
 
-(quit)
-" | $my_lisp
+#-allegro (quit)
+" | $my_lisp $args
 
 echo "Above test results for $my_lisp."
 
---
+++ test/usocket.asd
@@ -0,0 +1,1 @@
+../usocket.asd
