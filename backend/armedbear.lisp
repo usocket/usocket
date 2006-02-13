1a6bc1ba0eb129b1d001fc2f1d56cc4e2c151742
ehuelsmann <ehuelsmann@423ce6ab-4d0b-0410-a604-eef82375bc58>
2006-02-13 20:48:39 +0000

    Expand TODO.

--- backend/armedbear.lisp
+++ backend/armedbear.lisp
@@ -22,3 +22,33 @@
 (defmethod socket-close ((usocket usocket))
   (with-mapped-conditions (usocket)
     (ext:socket-close (socket usocket))))
+
+
+#.(if (find-symbol "SOCKET-LOCAL-ADDRESS" :ext)
+      ;; abcl 0.0.9 compat code
+      '(progn
+         (declaim (inline %socket-address %socket-port))
+         (defun %socket-address (socket addressName)
+           (java:jcall "getHostAddress" (java:jcall-raw addressName socket)))
+
+         (defun %socket-port (socket portName)
+           (java:jcall portName socket))
+
+
+
+         (defun socket-local-address (socket)
+           "Returns the local address of the given socket as a dotted quad string."
+           (%socket-address socket "getLocalAddress"))
+
+         (defun socket-peer-address (socket)
+           "Returns the peer address of the given socket as a dotted quad string."
+           (%socket-address socket "getInetAddress"))
+
+         (defun socket-local-port (socket)
+           "Returns the local port number of the given socket."
+           (%socket-port socket "getLocalPort"))
+
+         (defun socket-peer-port (socket)
+           "Returns the peer port number of the given socket."
+           (%socket-port socket "getPort")))
+    '(progn))
