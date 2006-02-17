aded1b5554a8ef3f73b6bfb674874a8bdbe55ef1
ehuelsmann <ehuelsmann@423ce6ab-4d0b-0410-a604-eef82375bc58>
2006-02-17 20:53:53 +0000

    Export new interfaces.

--- package.lisp
+++ package.lisp
@@ -10,23 +10,29 @@
 (eval-when (:execute :load-toplevel :compile-toplevel)
   (defpackage :usocket
       (:use :cl)
-    (:export :socket-connect ; socket constructors and methods
-             :socket-close
+    (:export #:socket-connect ; socket constructors and methods
+             #:socket-close
+             #:get-local-address
+             #:get-peer-address
+             #:get-local-port
+             #:get-peer-port
+             #:get-local-name
+             #:get-peer-name
 
-             :with-connected-socket ; macros
+             #:with-connected-socket ; macros
 
-             :usocket ; socket object and accessors
-             :socket
-             :socket-stream
+             #:usocket ; socket object and accessors
+             #:socket
+             #:socket-stream
 
-             :host-byte-order ; IPv4 utility functions
-             :hbo-to-dotted-quad
-             :hbo-to-vector-quad
-             :vector-quad-to-dotted-quad
-             :dotted-quad-to-vector-quad
+             #:host-byte-order ; IPv4 utility functions
+             #:hbo-to-dotted-quad
+             #:hbo-to-vector-quad
+             #:vector-quad-to-dotted-quad
+             #:dotted-quad-to-vector-quad
 
-             :socket-condition ; conditions
-             :socket-error ; errors
-             :unknown-condition
-             :unknown-error)))
+             #:socket-condition ; conditions
+             #:socket-error ; errors
+             #:unknown-condition
+             #:unknown-error)))
 
