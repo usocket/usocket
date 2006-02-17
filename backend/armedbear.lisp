9561c5eeaf5bd9c954e2b135a4a18c427e2823a4
ehuelsmann <ehuelsmann@423ce6ab-4d0b-0410-a604-eef82375bc58>
2006-02-17 20:27:51 +0000

    Remove non-working 0.0.9 compat code.

--- backend/armedbear.lisp
+++ backend/armedbear.lisp
@@ -24,48 +24,18 @@
     (ext:socket-close (socket usocket))))
 
 
-#.(if (null (find-symbol "SOCKET-LOCAL-ADDRESS" :ext))
-      ;; abcl 0.0.9 compat code
-      '(progn
-         (declaim (inline %socket-address %socket-port))
-         (defun %socket-address (socket addressName)
-           (java:jcall "getHostAddress" (java:jcall-raw addressName socket)))
-
-         (defun %socket-port (socket portName)
-           (java:jcall portName socket))
-
-
-
-         (defun socket-local-address (socket)
-           "Returns the local address of the given socket as a dotted quad string."
-           (%socket-address socket "getLocalAddress"))
-
-         (defun socket-peer-address (socket)
-           "Returns the peer address of the given socket as a dotted quad string."
-           (%socket-address socket "getInetAddress"))
-
-         (defun socket-local-port (socket)
-           "Returns the local port number of the given socket."
-           (%socket-port socket "getLocalPort"))
-
-         (defun socket-peer-port (socket)
-           "Returns the peer port number of the given socket."
-           (%socket-port socket "getPort")))
-    '(progn
-       (import (:socket-peer-port :socket-peer-address
-                :socket-local-port :socket-local-address) :ext)))
 
 (defmethod get-local-address ((usocket usocket))
-  (dotted-quad-to-vector-quad (socket-local-address (socket usocket))))
+  (dotted-quad-to-vector-quad (ext:socket-local-address (socket usocket))))
 
 (defmethod get-peer-address ((usocket usocket))
-  (dotted-quad-to-vector-quad (socket-peer-address (socket usocket))))
+  (dotted-quad-to-vector-quad (ext:socket-peer-address (socket usocket))))
 
 (defmethod get-local-port ((usocket usocket))
-  (socket-local-port (socket usocket)))
+  (ext:socket-local-port (socket usocket)))
 
 (defmethod get-peer-port ((usocket usocket))
-  (socket-peer-port (socket usocket)))
+  (ext:socket-peer-port (socket usocket)))
 
 (defmethod get-local-name ((usocket usocket))
   (values (get-local-address usocket)
