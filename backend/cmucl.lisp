bce22bc501da83ddcfe48efaf795f3271d6c7776
ehuelsmann <ehuelsmann@423ce6ab-4d0b-0410-a604-eef82375bc58>
2007-01-20 12:48:03 +0000

    Add last-minute :element-type specification for trivial-sockets compatibility.

--- backend/cmucl.lisp
+++ backend/cmucl.lisp
@@ -83,10 +83,11 @@
                                            (host-to-hbo host)))))))
    (make-stream-server-socket server-sock :element-type element-type)))
 
-(defmethod socket-accept ((usocket stream-server-usocket))
+(defmethod socket-accept ((usocket stream-server-usocket) &key element-type)
   (let* ((sock (ext:accept-tcp-connection (socket usocket)))
          (stream (sys:make-fd-stream sock :input t :output t
-                                     :element-type (element-type usocket)
+                                     :element-type (or element-type
+                                                       (element-type usocket))
                                      :buffering :full)))
     (make-stream-socket :socket sock :stream stream)))
 
