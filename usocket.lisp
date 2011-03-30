556d59e41427c57f2610d66d8d145114c2f47cd9
ctian <ctian@423ce6ab-4d0b-0410-a604-eef82375bc58>
2011-03-30 07:34:03 +0000

    Merge r607 from branch 0.5.x

--- usocket.lisp
+++ usocket.lisp
@@ -313,16 +313,16 @@ none."
 
 
 (defmacro port-to-octet-buffer (port buffer &key (start 0))
-  `(integer-to-octet-buffer ,port ,buffer 2 ,start))
+  `(integer-to-octet-buffer ,port ,buffer 2 :start ,start))
 
 (defmacro ip-to-octet-buffer (ip buffer &key (start 0))
-  `(integer-to-octet-buffer (host-byte-order ,ip) ,buffer 4 ,start))
+  `(integer-to-octet-buffer (host-byte-order ,ip) ,buffer 4 :start ,start))
 
 (defmacro port-from-octet-buffer (buffer &key (start 0))
-  `(octet-buffer-to-integer ,buffer 2 ,start))
+  `(octet-buffer-to-integer ,buffer 2 :start ,start))
 
 (defmacro ip-from-octet-buffer (buffer &key (start 0))
-  `(octet-buffer-to-integer ,buffer 4 ,start))
+  `(octet-buffer-to-integer ,buffer 4 :start ,start))
 
 ;;
 ;; IP(v4) utility functions
