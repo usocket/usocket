e26282ac03d4d5406cec008d0afeef0b6834f837
Felix Lange <fjl@twurst.com>
2021-08-01 02:03:23 +0200

    Clean up test server

--- test/test-timeout.lisp
+++ test/test-timeout.lisp
@@ -20,27 +20,25 @@
                      (format *error-output* "Client handler error: ~A~%" c)
                      (return-from tcp-echo-handler))))
     (loop with delay = 0 do
-      (let ((command (read-byte client nil nil)))
-        (unless command
+      (let ((cmd (read-byte client nil nil)))
+        (unless cmd
           (return)) ;; client disconnected
-        (let* ((length (read-byte client))
-               (buffer (make-array length :element-type '(unsigned-byte 8))))
-          (read-sequence buffer client)
-          (cond ((= command +cmd-echo+)
-                 (when (plusp delay)
-                   (sleep delay)
-                   (setf delay 0))
-                 (write-sequence buffer client))
-                ((= command +cmd-read+)
-                 (when (plusp delay)
-                   (sleep delay)
-                   (setf delay 0)))
-                ((= command +cmd-setdelay+)
-                 (unless (= length 1)
-                   (error "Bad setdelay data length ~A, want 1 byte" length))
-                 (setf delay (aref buffer 0)))
-                (t
-                 (error "Unknown command ~A" command))))))))
+        (let* ((length (read-byte client)))
+          (cond
+            ((or (= cmd +cmd-echo+) (= cmd +cmd-read+))
+             (when (plusp delay)
+               (sleep delay)
+               (setf delay 0))
+             (let ((buffer (make-array length :element-type '(unsigned-byte 8))))
+               (read-sequence buffer client)
+               (when (= cmd +cmd-echo+)
+                 (write-sequence buffer client))))
+            ((= cmd +cmd-setdelay+)
+             (unless (= length 1)
+               (error "Bad setdelay data length ~A, want 1 byte" length))
+             (setf delay (read-byte client)))
+            (t
+             (error "Unknown command ~A" cmd))))))))
 
 (defun send-tcp-echo-command (socket command &optional data)
   (assert (< (length data) 256))
